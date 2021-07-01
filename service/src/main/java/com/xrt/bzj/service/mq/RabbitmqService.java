addaa289a576f7628a5430f23b136083eac819d7
leehaitao <15815542272@163.com>
2021-07-01 23:51:13 +0800

    [feat] 添加rabbitMQ代码

---
+++ service/src/main/java/com/xrt/bzj/service/mq/RabbitmqService.java
@@ -0,0 +1,12 @@
+package com.xrt.bzj.service.mq;
+
+/**
+ * @author Lee
+ * @version 1.0
+ * @project demo
+ * @package com.xrt.bzj.service.mq
+ * @date 2021/7/1 22:27
+ */
+public interface RabbitmqService {
+    void sendWork();
+}
---
+++ service/src/main/java/com/xrt/bzj/service/mq/impl/RabbitmqServiceImpl.java
@@ -0,0 +1,24 @@
+package com.xrt.bzj.service.mq.impl;
+
+import com.xrt.bzj.service.mq.RabbitmqService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Lee
+ * @version 1.0
+ * @project demo
+ * @package com.xrt.bzj.service.mq.impl
+ * @date 2021/7/1 22:31
+ */
+@Service
+public class RabbitmqServiceImpl implements RabbitmqService {
+
+    @Autowired
+    RabbitmqMapper rabbitmqMapper;
+
+    @Override
+    public void sendWork() {
+        rabbitmqMapper.sendWork();
+    }
+}
